c259c2e1766888ff61cbc531d793c700b0cae7fa
Andri Sedania <andri@sedaniainnovator.com>
2020-04-19 16:56:50 +0700

    10 - Display Users in Table and Modal Window

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -88,7 +88,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <ul class="nav nav-treeview">
               <li class="nav-item">
                 <router-link to="/users" class="nav-link">
-                  <i class="fas fa-users nav-icon yellow"></i>
+                  <i class="fas fa-users nav-icon"></i>
                   <p>Users</p>
                 </router-link>
               </li>
