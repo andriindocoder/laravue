703ade7773718a089a8c95f7536bb3040fa300cb
Andri Sedania <andri@sedaniainnovator.com>
2020-05-02 16:32:14 +0700

    29 - User Profile Component

--- resources/js/components/Profile.vue
+++ resources/js/components/Profile.vue
@@ -71,13 +71,13 @@
                           <div class="form-group row">
                             <label for="inputName" class="col-sm-2 col-form-label">Name</label>
                             <div class="col-sm-10">
-                              <input type="email" class="form-control" id="inputName" placeholder="Name">
+                              <input v-model="form.name" type="name" class="form-control" id="inputName" placeholder="Name">
                             </div>
                           </div>
                           <div class="form-group row">
                             <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
                             <div class="col-sm-10">
-                              <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+                              <input v-model="form.email" type="email" class="form-control" id="inputEmail" placeholder="Email">
                             </div>
                           </div>
                           <div class="form-group row">
@@ -126,8 +126,25 @@
 
 <script>
     export default {
+        data() {
+            return {
+                form: new Form({
+                    id: '',
+                    name: '',
+                    email: '',
+                    password: '',
+                    type: '',
+                    bio: '',
+                    photo: ''
+                })
+            }
+        },
         mounted() {
             console.log('Component mounted.')
+        },
+        created() {
+            axios.get('api/profile')
+            .then(({data}) => (this.form.fill(data)));
         }
     }
 </script>
