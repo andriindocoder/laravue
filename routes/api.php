c821d016ad129f691afed7385e8a452c8b3c90ce
andri <andriwicaksonost@gmail.com>
2020-06-03 17:23:51 +0700

    Controller and api route

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// List articles
+Route::get('articles', 'ArticleController@index');
+
+// List single article
+Route::get('article/{id}', 'ArticleController@show');
+
+Route::post('article', 'ArticleController@store');
+Route::put('article', 'ArticleController@store');
+Route::delete('article', 'ArticleController@destroy');
