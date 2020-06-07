ca0c534621f93b3179950dfaa31797d0cb3cb92a
andri <andriwicaksonost@gmail.com>
2020-06-07 20:11:41 +0700

    Laravel Vue

---
+++ resources/js/components/ArticleComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header"><h3>Article Compoment</h3></div>
+
+                    <div class="card-body">
+                        Article component mounted.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
