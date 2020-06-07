6a6b623efb50fc6404eb04b807b523ffe883823d
andri <andriwicaksonost@gmail.com>
2020-06-07 20:37:32 +0700

    Pagination

--- resources/js/components/NavbarComponent.vue
+++ resources/js/components/NavbarComponent.vue
@@ -1,5 +1,5 @@
 <template>
-	<nav class="navbar navbar-expand-sm navbar-dark bg-info mb-5">
+	<nav class="navbar navbar-expand-sm navbar-dark bg-info mb-3">
 		<div class="container">
 			<a href="#" class="navbar-brand">Laravue</a>
 		</div>
